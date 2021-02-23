b37fa8995628d65a38d58b79d3f1ef6f4b177370
Piero Mamberti <piero.mamberti@gmail.com>
2021-02-23 18:09:29 +0000

    Add Multiply

--- calculator_test.go
+++ calculator_test.go
@@ -22,3 +22,13 @@ func TestSubtract(t *testing.T) {
 		t.Errorf("want %f, got %f", want, got)
 	}
 }
+
+func TestMultiply(t *testing.T) {
+	t.Parallel()
+	var want float64 = 88
+	got := calculator.Multiply(8, 11)
+	if want != got {
+		t.Errorf("want %f, got %f", want, got)
+	}
+
+}
